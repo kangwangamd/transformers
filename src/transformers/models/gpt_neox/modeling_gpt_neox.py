68d9e9ebdeeceb9cb40cc3783627536e95c2dea2
kangwangamd <100359556+kangwangamd@users.noreply.github.com>
2023-08-10 11:05:35 +0800

    Update modeling_gpt_neox.py
    apply half to rotary_pos_emb

--- src/transformers/models/gpt_neox/modeling_gpt_neox.py
+++ src/transformers/models/gpt_neox/modeling_gpt_neox.py
@@ -425,6 +425,8 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids):
     gather_indices = gather_indices.repeat(1, cos.shape[1], 1, cos.shape[3])
     cos = torch.gather(cos.repeat(gather_indices.shape[0], 1, 1, 1), 2, gather_indices)
     sin = torch.gather(sin.repeat(gather_indices.shape[0], 1, 1, 1), 2, gather_indices)
+    cos = cos.half()
+    sin = sin.half()
     q_embed = (q * cos) + (rotate_half(q) * sin)
     k_embed = (k * cos) + (rotate_half(k) * sin)
     return q_embed, k_embed
